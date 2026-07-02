e5a12a11e0f05c344a3505299a16f682407d2c55
Epixness <superhenk4@gmail.com>
2026-07-02 14:44:01 +0200

    try fix

--- pi-code/app.py
+++ pi-code/app.py
@@ -17,12 +17,10 @@ except ImportError:
 
 try:
     from gpiozero import AngularServo
-    from gpiozero.pins.lgpio import LGPIOPinFactory
     GPIOZERO_AVAILABLE = True
 except ImportError:
     GPIOZERO_AVAILABLE = False
     AngularServo = None
-    LGPIOPinFactory = None
 
 load_dotenv()
 
@@ -59,10 +57,8 @@ def create_servo(name, gpio, min_pw=SERVO_MIN_PWM, max_pw=SERVO_MAX_PWM, open_an
         app.logger.warning('[servo:%s] gpiozero not available on this system', name)
         return False
     try:
-        factory = LGPIOPinFactory() if LGPIOPinFactory else None
         sns = AngularServo(
             gpio,
-            pin_factory=factory,
             min_pulse_width=min_pw,
             max_pulse_width=max_pw
         )
